dedee364b2f5761f8bde2dfc1d51c2a3b5d4953b
lsy <923372823@qq.com>
2021-05-18 17:35:06 +0800

    分类员分类号静态值

--- src/main/java/com/kfgs/aotc/util/CountUtil.java
+++ src/main/java/com/kfgs/aotc/util/CountUtil.java
@@ -1,33 +1,56 @@
 package com.kfgs.aotc.util;
 
+import com.kfgs.aotc.pojo.business.ClassifierInfo;
+import com.kfgs.aotc.pojo.business.DetailsOfTheCase;
 import com.kfgs.aotc.repository.ClassifierInfoRepository;
+import com.kfgs.aotc.repository.DetailsOfTheCaseRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
 
-import java.util.HashMap;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletContext;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class CountUtil {
     @Autowired
-    private static ClassifierInfoRepository classifierRepository;
+    private ClassifierInfoRepository classifierRepository;
 
-    public static HashMap<String,String> getClassificationCode(){
-        HashMap<String,String> map = new HashMap<>();
-        return map;
-    }
+    @Autowired
+    private DetailsOfTheCaseRepository detailsOfTheCaseRepository;
+    /**
+     * 分类员与其对应分类号
+     * @return
+     */
+    public static Map<String,List<String>> CLASSIFIERS_AND_CODE = new HashMap<>();
+    //public static Map<String,List<String>> FINISHED_CASE_CODE = new HashMap<>();
 
-    public static HashMap<String,String> getClassificationName(){
-        HashMap<String,String> map = new HashMap<>();
-        return map;
-    }
-
-    /*public static HashMap<String, ClassifierInfo> getUserInfo(){
-        HashMap<String,ClassifierInfo> map = new HashMap<>();
+    @PostConstruct
+    public void init(){
+        System.out.println("加载classifiersCodeMap......");
         List<ClassifierInfo> list = classifierRepository.findAll();
         for (int i=0;i<list.size();i++){
-            String classifierCode = list.get(i).getClassifiersCode().toString();
-            map.put(classifierCode,list.get(i));
+            String classifierCode = list.get(i).getClassifiersCode();
+            List<String> classCodes = new ArrayList<>();
+            String[] codes = list.get(i).getClassificationField().split(",");
+            for (int j=0;j<codes.length;j++){
+                if (codes[j].length()>=4){
+                    classCodes.add(codes[j].substring(0,4));
+                }
+            }
+            CLASSIFIERS_AND_CODE.put(classifierCode,classCodes);
         }
-        return map;
-    }*/
+
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("运行结束");
+    }
+
 }
