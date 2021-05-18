23d2567df811941cf844864046cbf4865eadee35
lsy <923372823@qq.com>
2021-05-18 17:52:03 +0800

    出案

---
+++ src/main/java/com/kfgs/aotc/repository/DetailsOfTheCaseRepository.java
@@ -0,0 +1,9 @@
+package com.kfgs.aotc.repository;
+
+import com.kfgs.aotc.common.repository.CommonRepository;
+import com.kfgs.aotc.pojo.business.DetailsOfTheCase;
+
+public interface DetailsOfTheCaseRepository extends CommonRepository<DetailsOfTheCase,String> {
+
+
+}
