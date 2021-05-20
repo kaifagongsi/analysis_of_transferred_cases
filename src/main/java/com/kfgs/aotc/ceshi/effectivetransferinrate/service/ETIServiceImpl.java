106ab1d04d02f68a470abd170490bd2bef99218f
mango_liang <mango_liang@outlook.com>
2021-05-20 10:39:33 +0800

    转入率添加部级和个人，并完成分页操作

--- src/main/java/com/kfgs/aotc/ceshi/effectivetransferinrate/service/ETIServiceImpl.java
+++ src/main/java/com/kfgs/aotc/ceshi/effectivetransferinrate/service/ETIServiceImpl.java
@@ -1,10 +1,15 @@
 package com.kfgs.aotc.ceshi.effectivetransferinrate.service;
 
+import com.kfgs.aotc.common.pojo.PageCondition;
+import com.kfgs.aotc.common.pojo.PageInfo;
 import com.kfgs.aotc.common.pojo.Result;
+import com.kfgs.aotc.pojo.business.ClassifierInfo;
 import com.kfgs.aotc.pojo.business.vo.ParameterVo;
+import com.kfgs.aotc.repository.ClassifierInfoRepository;
 import com.kfgs.aotc.repository.TransferProcessRepository;
 import com.kfgs.aotc.util.CountUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,26 +24,62 @@ public class ETIServiceImpl implements ETIService{
     @Autowired
     TransferProcessRepository transferProcessRepository;
 
+    @Autowired
+    ClassifierInfoRepository classifierInfoRepository;
+
     @Override
     public Result getEffectiveTransferInRate(ParameterVo parameterVo) {
         switch (parameterVo.getFirstClassify()){
             case 1:
-                return getEffectiveTransferInRateByPeople(parameterVo.getStartDate(),parameterVo.getEndDate(),parameterVo.getSecondClassify());
+                return getEffectiveTransferInRateByPeople(parameterVo);
                  //return getEffectiveTransferInRateByPeople(parameterVo);
             case 2:
-                System.out.println("2");
-                break;
+                return getEffectiveTransferInRateByDep1(parameterVo);
             default:
 
         }
-
-
-
         return null;
     }
 
-    private Result getEffectiveTransferInRateByPeople(String startDate,String endDate,String classifierID){
+    private Result getEffectiveTransferInRateByDep1( ParameterVo parameterVo) {
         List<Map<String,String>> resultList = new ArrayList<>();
+
+        DecimalFormat df = (DecimalFormat) NumberFormat.getInstance();
+        PageCondition page = (PageCondition)parameterVo;
+        Page<ClassifierInfo> classifierCode = classifierInfoRepository.findClassifiersCodeByDep1(parameterVo.getSecondClassify(),page.getPageable());
+        for(ClassifierInfo  id : classifierCode.getContent()){
+            resultList.add(getPeople(parameterVo.getStartDate(),parameterVo.getEndDate(),id.getClassifiersCode(),id.getEname()));
+        }
+        PageInfo pageInfo = PageInfo.ofMap(classifierCode,resultList);
+        Result<PageInfo> of = Result.of(pageInfo);
+        return of;
+    }
+
+    /**
+     * 个人转入率
+     * @param parameterVo
+     * @return
+     */
+    private Result getEffectiveTransferInRateByPeople( ParameterVo parameterVo){
+        List resultList = new ArrayList<>();
+        PageCondition page = (PageCondition)parameterVo;
+        Page<ClassifierInfo> classifierCode = classifierInfoRepository.findClassifiersCodeByClassifierCode(parameterVo.getSecondClassify() ,page.getPageable());
+        for(ClassifierInfo  id : classifierCode.getContent()){
+            resultList.add(getPeople(parameterVo.getStartDate(),parameterVo.getEndDate(),id.getClassifiersCode(),id.getEname()));
+        }
+        PageInfo pageInfo = PageInfo.ofMap(classifierCode,resultList);
+        Result<PageInfo> of = Result.of(pageInfo);
+        return of;
+    }
+
+    /**
+     * 返回单个人的比例
+     * @param startDate 开始时间
+     * @param endDate 结束时间
+     * @param classifierID 人员ID
+     * @return
+     */
+    private LinkedHashMap getPeople(String startDate,String endDate,String classifierID,String ename ){
         LinkedHashMap<String,String> result = new LinkedHashMap<>();
         DecimalFormat df = (DecimalFormat) NumberFormat.getInstance();
         double accuracy_num = 0;//有效转案率
@@ -46,6 +87,7 @@ public class ETIServiceImpl implements ETIService{
         int fengmu = transferProcessRepository.getAcceptReferralCountNumberByReceiveTimeBetweenAndReceiveId(startDate, endDate,classifierID);
         if(fengmu ==0){
             result.put("分类员代码",classifierID);
+            result.put("分类员姓名",ename);
             result.put("转案总次数","0");
             result.put("转案接收总次数", "0");
             result.put("转案退回且出案数","0");
@@ -74,10 +116,10 @@ public class ETIServiceImpl implements ETIService{
                 if (!Collections.disjoint(Arrays.asList(finishCodes),detailCodes)){
                     validTrans ++;
                 }
-
             }
             accuracy_num = (case_1 + validTrans)*100/fengmu;
             result.put("分类员代码",classifierID);
+            result.put("分类员姓名",ename);
             result.put("转案总次数",Integer.toString(fengmu));
             result.put("转案接收总次数", Integer.toString(case_1));
             result.put("转案退回且出案数",Integer.toString( ipc.size()));
@@ -85,15 +127,6 @@ public class ETIServiceImpl implements ETIService{
             result.put("有效转案率",df.format(accuracy_num)+"%");
             System.out.println(result);
         }
-        resultList.add(result);
-        return Result.of(resultList);
-
-        /*Page<TransferProcess> andTipeTitle = transferProcessRepository.getAllConditionsByReceiveTimeAndTipeTitle(parameterVo.getStartDate(), parameterVo.getEndDate(),"接收转案",page.getPageable());
-
-        //Page<TransferProcess> allConditions = transferProcessRepository.findAllConditionsByReceiveTimeAndTipeTitle(parameterVo.getStartDate(), parameterVo.getEndDate(),"接收转案",page.getPageable());
-        //2 按照处理转案时间、提示状态为拒绝转案的分页集合
-        //transferProcessRepository.findAllBy
-        System.out.println(andTipeTitle);
-        return Result.of(andTipeTitle);*/
+        return result;
     }
 }
