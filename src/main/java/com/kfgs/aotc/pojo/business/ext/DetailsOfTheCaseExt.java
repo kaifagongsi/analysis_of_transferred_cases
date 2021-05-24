d344b6c7d25c17d6941da4d1f8ca95a2cebe3473
mango_liang <mango_liang@outlook.com>
2021-05-24 17:36:01 +0800

    案件信息的扩展

---
+++ src/main/java/com/kfgs/aotc/pojo/business/ext/DetailsOfTheCaseExt.java
@@ -0,0 +1,24 @@
+package com.kfgs.aotc.pojo.business.ext;
+
+import com.kfgs.aotc.pojo.business.DetailsOfTheCase;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+@Data
+public class DetailsOfTheCaseExt extends DetailsOfTheCase implements Serializable {
+    // ipcmi + ipcoi + ipca
+    private String ipc;
+    // 接受转案的人员 对应的是aotc_transfer_process 中的 RECEIVE_ID
+    private String receiveId;
+    //
+    private String caseId;
+
+    public DetailsOfTheCaseExt(String ipc, String receiveId,String caseId) {
+        this.ipc = ipc;
+        this.receiveId = receiveId;
+        this.caseId = caseId;
+    }
+}
---
+++ src/main/java/com/kfgs/aotc/repository/DetailsOfTheCaseExtRepository.java
@@ -0,0 +1,22 @@
+package com.kfgs.aotc.repository;
+
+import com.kfgs.aotc.common.repository.CommonRepository;
+import com.kfgs.aotc.pojo.business.ext.DetailsOfTheCaseExt;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DetailsOfTheCaseExtRepository extends CommonRepository<DetailsOfTheCaseExt,String> {
+
+
+
+    @Query(nativeQuery = true,value = " select  dotc.ipcmi || ',' || dotc.ipcoi || ',' || dotc.ipca  as ipc,tp.receive_id as receiveId, dotc.case_id as caseId  from aotc_detailsofthecase dotc  left join    aotc_transfer_process  tp " +
+            " on dotc.case_id = tp.case_id  " +
+            " where tp.receive_time between ?1 and ?2 " +
+            " and tp.tips_state = ?3 " +
+            " and dotc.case_id = tp.case_id  " +
+            " and tp.receive_id in (?4)  ")
+    List<Object[]> getRefuseReferralByReceiveTimeBetweenAndTipeStateAndReceiveIds(String startDate, String endDate, String tipsState, List classifierInfoCode);
+
+
+}
