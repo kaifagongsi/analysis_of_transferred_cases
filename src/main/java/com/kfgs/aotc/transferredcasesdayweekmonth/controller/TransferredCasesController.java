a7c70ac4c55ac810e1b1f41b45e7d9213da9b6c3
mango_liang <mango_liang@outlook.com>
2021-12-24 09:19:50 +0800

    修复月、周、日的bug

--- src/main/java/com/kfgs/aotc/transferredcasesdayweekmonth/controller/TransferredCasesController.java
+++ src/main/java/com/kfgs/aotc/transferredcasesdayweekmonth/controller/TransferredCasesController.java
@@ -4,11 +4,15 @@ import com.kfgs.aotc.common.pojo.Result;
 import com.kfgs.aotc.pojo.business.vo.ParameterVo;
 import com.kfgs.aotc.transferredcasesdayweekmonth.service.ITransferredCasesService;
 import com.kfgs.aotc.transferredcasesdayweekmonth.service.TransferredCasesFactory;
+import com.kfgs.aotc.util.DateUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 /**
  * 处理转案率，日、周、月
@@ -25,8 +29,30 @@ public class TransferredCasesController {
 
 
     @PostMapping("init/{type}")
-    public Result getTransferredCasesInit(@PathVariable String type,  ParameterVo parameterVo){
+    public Result getTransferredCasesInit(@PathVariable String type,  ParameterVo parameterVo) throws ParseException {
         ITransferredCasesService service = TransferredCasesFactory.createObj(type);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+        // 如果是周的话判断开始日期是否为周一，结束日期是否为周日
+        if("week".equalsIgnoreCase(type)){
+            String startDate = parameterVo.getStartDate();
+            String endDate = parameterVo.getEndDate();
+            String formatStartDate = sdf.format(DateUtil.getThisWeekMonday(sdf.parse(startDate)));
+            if(!startDate.equalsIgnoreCase(formatStartDate)){
+                return Result.of(null,false,"按周统计时，开始日期请选择周一");
+            }
+            if(! DateUtil.isLastDayOfWeek(sdf.parse(endDate))){
+                return Result.of(null,false,"按周统计时，结束日期请选择周日");
+            }
+        }
+        if("month".equalsIgnoreCase(type)){
+            boolean firstDayOfMonth = DateUtil.isFirstDayOfMonth(sdf.parse(parameterVo.getStartDate()));
+            if(!firstDayOfMonth){
+                return Result.of(null,false,"按月统计时，开始日期请选择本月一号");
+            }
+            if(!DateUtil.isLastDayOfMonth(sdf.parse(parameterVo.getEndDate()))){
+                return Result.of(null,false,"按月统计时，开始日期请选择本月最后一天");
+            }
+        }
         return service.calculation(parameterVo);
     }
 
--- src/main/java/com/kfgs/aotc/transferredcasesdayweekmonth/service/TransferredCasesFactory.java
+++ src/main/java/com/kfgs/aotc/transferredcasesdayweekmonth/service/TransferredCasesFactory.java
@@ -21,7 +21,8 @@ public class TransferredCasesFactory {
 
     public  void  init(){
         // 用于放置一些初始化，根据需要选择取舍
-        list.clear(); // 每次初始化先清除旧数据
+        // 每次初始化先清除旧数据
+        list.clear();
         list.add((ITransferredCasesService)applicationContext.getBean("transferredCasesServiceByDay")); // 这里根据条件判断优先级放入目标对象,先放入的先判断
         list.add((ITransferredCasesService)applicationContext.getBean("transferredCasesServiceByWeek"));
         list.add((ITransferredCasesService)applicationContext.getBean("transferredCasesServiceByMonth"));
@@ -32,9 +33,11 @@ public class TransferredCasesFactory {
         for(int i=0;i<list.size();i++){
             p = list.get(i);
             if(p.choseCurrent(day)){
-                return p; // 如果当前条件满足，则直接返回目标对象
+                // 如果当前条件满足，则直接返回目标对象
+                return p;
             }
         }
-        return new TransferredCasesServiceByDay();// 别忘了最后返回一个默认值，对应于最后一个else判断
+        // 别忘了最后返回一个默认值，对应于最后一个else判断
+        return new TransferredCasesServiceByDay();
     }
 }
